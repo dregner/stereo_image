c9cb53d284f0eab612ee8d7015bf50903fec4089
Daniel Regner <daniel_regner@hotmail.com>
2022-08-17 14:54:42 -0300

    create function to save three images

--- src/show_stereo_fpv.cpp
+++ src/show_stereo_fpv.cpp
@@ -6,7 +6,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <fstream>
 #include <image_transport/image_transport.h>
-
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/opencv.hpp>
 
 using namespace sensor_msgs;
 
@@ -26,20 +27,12 @@ std::string decimal(int r);
 static std::ofstream images_file;
 
 void callback(const sensor_msgs::ImageConstPtr &image_R,
-              const sensor_msgs::ImageConstPtr &image_L) {
+              const sensor_msgs::ImageConstPtr &image_L,
+              const sensor_msgs::ImageConstPtr &image_fpv) {
 
     cv_bridge::CvImagePtr cv_ptr_R;
     cv_bridge::CvImagePtr cv_ptr_L;
-
-    // Renaming images to the correct format.
-    std::string img_counter = "";
-    if (counter < 10) {
-        img_counter = "000";
-    } else if (counter > 9 && counter < 100) {
-        img_counter = "00";
-    } else if (counter < 99 && counter > 1000){
-        img_counter = "0";
-    }
+    cv_bridge::CvImagePtr cv_ptr_FPV;
 
 
     namespace enc = sensor_msgs::image_encodings;
@@ -47,39 +40,31 @@ void callback(const sensor_msgs::ImageConstPtr &image_R,
     try {
         cv_ptr_R = cv_bridge::toCvCopy(image_R, sensor_msgs::image_encodings::BGR8);
         cv_ptr_L = cv_bridge::toCvCopy(image_L, sensor_msgs::image_encodings::BGR8);
+        cv_ptr_FPV = cv_bridge::toCvCopy(image_fpv, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception &e) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
     }
+   cv::imshow("fpv",cv_ptr_FPV->image);
+   cv::waitKey(1);
 
-    std::stringstream writeR;
-    std::stringstream writeL;
-    writeR << "R" << img_counter << counter << ".png";
-    writeL << "L" << img_counter << counter << ".png";
-    cv::imwrite(writeL.str(), cv_ptr_L->image);
-    cv::imwrite(writeR.str(), cv_ptr_R->image);
-    std::cout << "Aqcuired Image -" << counter << std::endl;
-
-    std::cout << "Press Enter to Continue";
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-
-    ++counter;
 }
 
 int main(int argc, char **argv) {
 
-    ros::init(argc, argv, "stereo_thread");
+    ros::init(argc, argv, "show_fpv");
     ros::NodeHandle nh;
 
     message_filters::Subscriber<sensor_msgs::Image> image_sub_R(nh, "/dji_osdk_ros/stereo_vga_front_right_images", 1);
     message_filters::Subscriber<sensor_msgs::Image> image_sub_L(nh, "/dji_osdk_ros/stereo_vga_front_left_images", 1);
+    message_filters::Subscriber<sensor_msgs::Image> image_sub_FPV(nh, "/dji_osdk_ros/fpv_camera_images", 1);
 
 
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
     // ExactTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(1), image_sub_R, image_sub_L);
-    sync.registerCallback(boost::bind(&callback, _1, _2));
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(5), image_sub_R, image_sub_L, image_sub_FPV);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 /*
     while(ros::ok()) {
         ros::spinOnce();
--- src/stereo_fpv_acquisition.cpp
+++ src/stereo_fpv_acquisition.cpp
@@ -26,10 +26,12 @@ std::string decimal(int r);
 static std::ofstream images_file;
 
 void callback(const sensor_msgs::ImageConstPtr &image_R,
-              const sensor_msgs::ImageConstPtr &image_L) {
+              const sensor_msgs::ImageConstPtr &image_L,
+              const sensor_msgs::ImageConstPtr &image_fpv) {
 
     cv_bridge::CvImagePtr cv_ptr_R;
     cv_bridge::CvImagePtr cv_ptr_L;
+    cv_bridge::CvImagePtr cv_ptr_FPV;
 
     // Renaming images to the correct format.
     std::string img_counter = "";
@@ -47,6 +49,7 @@ void callback(const sensor_msgs::ImageConstPtr &image_R,
     try {
         cv_ptr_R = cv_bridge::toCvCopy(image_R, sensor_msgs::image_encodings::BGR8);
         cv_ptr_L = cv_bridge::toCvCopy(image_L, sensor_msgs::image_encodings::BGR8);
+        cv_ptr_FPV = cv_bridge::toCvCopy(image_fpv, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception &e) {
         ROS_ERROR("cv_bridge exception: %s", e.what());
@@ -55,10 +58,15 @@ void callback(const sensor_msgs::ImageConstPtr &image_R,
 
     std::stringstream writeR;
     std::stringstream writeL;
+    std::stringstream writeFPV;
     writeR << "R" << img_counter << counter << ".png";
     writeL << "L" << img_counter << counter << ".png";
+    writeFPV << "FPV" << img_counter << counter << ".png";
+
     cv::imwrite(writeL.str(), cv_ptr_L->image);
     cv::imwrite(writeR.str(), cv_ptr_R->image);
+    cv::imwrite(writeFPV.str(), cv_ptr_FPV->image);
+
     std::cout << "Aqcuired Image -" << counter << std::endl;
 
     std::cout << "Press Enter to Continue";
@@ -69,17 +77,18 @@ void callback(const sensor_msgs::ImageConstPtr &image_R,
 
 int main(int argc, char **argv) {
 
-    ros::init(argc, argv, "stereo_thread");
+    ros::init(argc, argv, "stereo_fpv_save");
     ros::NodeHandle nh;
 
     message_filters::Subscriber<sensor_msgs::Image> image_sub_R(nh, "/dji_osdk_ros/stereo_vga_front_right_images", 1);
     message_filters::Subscriber<sensor_msgs::Image> image_sub_L(nh, "/dji_osdk_ros/stereo_vga_front_left_images", 1);
+    message_filters::Subscriber<sensor_msgs::Image> image_sub_FPV(nh, "/dji_osdk_ros/fpv_camera_images", 1);
 
 
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
     // ExactTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(1), image_sub_R, image_sub_L);
-    sync.registerCallback(boost::bind(&callback, _1, _2));
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(5), image_sub_R, image_sub_L, image_sub_FPV);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 /*
     while(ros::ok()) {
         ros::spinOnce();
